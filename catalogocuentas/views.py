e718911e5290ce7f9cc8fb39ef68412fbfae9954
Cesar Morales <morpac3@gmail.com>
2024-11-04 21:59:22 -0600

    Actualizado catalogo de cuentas v2.0 Beta

--- catalogocuentas/views.py
+++ catalogocuentas/views.py
@@ -4,12 +4,12 @@ from .models import Cuenta
 # Create your views here.
 def catalogoCuentas(request):
     #cuentas = Cuenta.objects.all()
-    activos = Cuenta.objects.filter(clase='1')  
-    pasivos = Cuenta.objects.filter(clase='2')  
-    patrimonio = Cuenta.objects.filter(clase='3')  
-    resultado_deudor = Cuenta.objects.filter(clase='4')  
-    resultado_acreedor = Cuenta.objects.filter(clase='5')  
-    cuentas_cierre = Cuenta.objects.filter(clase='6')
+    activos = Cuenta.objects.filter(codClase='1')  
+    pasivos = Cuenta.objects.filter(codClase='2')  
+    patrimonio = Cuenta.objects.filter(codClase='3')  
+    resultado_deudor = Cuenta.objects.filter(codClase='4')  
+    resultado_acreedor = Cuenta.objects.filter(codClase='5')  
+    cuentas_cierre = Cuenta.objects.filter(codClase='6')
     return render(request, 'catalogocuentas.html',{
         'activos': activos,
         'pasivos': pasivos,

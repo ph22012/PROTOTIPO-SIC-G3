ba68fb050e857f8d2597bf1027d7c19dd3871051
Cesar Morales <morpac3@gmail.com>
2024-11-04 21:36:25 -0600

    Catálogo de cuentas v1.0 Beta

--- catalogocuentas/urls.py
+++ catalogocuentas/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('catalogo/', views.catalogoCuentas),
+    path('catalogo/', views.catalogoCuentas, name='catalogoCuentas'),
 ]
--- catalogocuentas/views.py
+++ catalogocuentas/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from .models import Cuenta
 
 # Create your views here.
 def catalogoCuentas(request):
-    return render(request, 'catalogocuentas.html')
+    #cuentas = Cuenta.objects.all()
+    activos = Cuenta.objects.filter(clase='1')  
+    pasivos = Cuenta.objects.filter(clase='2')  
+    patrimonio = Cuenta.objects.filter(clase='3')  
+    resultado_deudor = Cuenta.objects.filter(clase='4')  
+    resultado_acreedor = Cuenta.objects.filter(clase='5')  
+    cuentas_cierre = Cuenta.objects.filter(clase='6')
+    return render(request, 'catalogocuentas.html',{
+        'activos': activos,
+        'pasivos': pasivos,
+        'patrimonio': patrimonio,
+        'resultado_deudor': resultado_deudor,
+        'resultado_acreedor': resultado_acreedor,
+        'cuentas_cierre': cuentas_cierre,
+    })
+
+#{'cuentas': cuentas}

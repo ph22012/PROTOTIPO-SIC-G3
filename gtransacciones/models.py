d66f96170a763f798ff5a89bfb6d8be1f9f200e7
Cesar Morales <morpac3@gmail.com>
2024-11-05 17:02:04 -0600

    Registro de transacciones v2.0 beta

--- gtransacciones/models.py
+++ gtransacciones/models.py
@@ -1,13 +1,40 @@
 from django.db import models
 
-# Create your models here.
+class periodos(models.Model):
+    idPeriodo = models.AutoField(primary_key=True)
+    fechaInicio = models.DateField()
+    fechaFin = models.DateField()
+
+    class Meta: 
+        db_table = 'periodos'
+        managed = False
+        
+    def __str__(self):
+        return str(self.idPeriodo)
+
+class Cuenta(models.Model):
+    idCuenta = models.AutoField(unique=True, primary_key=True)
+    codClase = models.IntegerField()
+    rubro = models.IntegerField()
+    codMayor = models.IntegerField()
+    codCuenta = models.TextField()
+    nameCuenta = models.TextField()
+    codDetalle = models.TextField(unique=True)
+
+    class Meta:
+        db_table = 'cuentas'
+        managed = False
+
+    def __str__(self):
+        return self.nameCuenta
+
+# MODELO DE REGISTRO DE TRANSACCIÓN   
 class Transaction(models.Model):
-    idTransaccion = models.AutoField(unique=True, primary_key=True)
+    idTransaccion = models.AutoField(primary_key=True)
+    #idPeriodo = models.ForeignKey('periodos', on_delete=models.CASCADE)
     numPartida = models.CharField(max_length=50) 
-    idCuenta = models.ForeignKey('Cuenta', on_delete=models.CASCADE) 
-    """monto_cargado = models.DecimalField(max_digits=10, decimal_places=2)  
-    monto_abonado = models.DecimalField(max_digits=10, decimal_places=2)"""
-    idSaldoTransaccion = models.ForeignKey('SaldoTransaccion', on_delete=models.CASCADE)
+    idCuenta = models.ForeignKey(Cuenta, on_delete=models.CASCADE)
+    monto = models.DecimalField(max_digits=10, decimal_places=2)   
     descripcion = models.TextField()
     fecha = models.DateField()
 
@@ -16,13 +43,206 @@ class Transaction(models.Model):
         ordering = ['fecha']
 
     def __str__(self):
-        return f'Transacción {self.idTransaccion} - {self.descripcion}'
+        return f'Transacción {self.idTransaccion}'
+
+    def registrar_saldo(self, id_cuenta, monto, tipo, fecha):
+        saldo_transaccion = SaldosTransaccion(
+            idCuenta=id_cuenta,
+            idTransaccion=self,
+            fecha=fecha
+        )
+        if tipo.lower() == 'cargo':
+            saldo_transaccion.monto_cargo = monto
+        elif tipo.lower() == 'haber':
+            saldo_transaccion.monto_haber = monto
+        else:
+            raise ValueError("El tipo de saldo debe ser 'cargo' o 'haber'.")
+
+        saldo_transaccion.save()
+
+class SaldosTransaccion(models.Model):
+    idSaldoTransaccion = models.AutoField(primary_key=True)
+    idCuenta = models.ForeignKey(Cuenta, on_delete=models.CASCADE)
+    idTransaccion = models.ForeignKey(Transaction, on_delete=models.CASCADE)
+    monto_cargo = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    monto_haber = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    fecha = models.DateField()
+
+    class Meta:
+        db_table = 'gtransacciones_saldostransaccion'
+
+    def __str__(self):
+        return f'Saldo {self.idSaldoTransaccion} - Cuenta: {self.idCuenta}'
 
 
 
 
 
-"""date = models.DateField()
+
+
+
+"""from django.db import models
+
+class periodos(models.Model) :
+    idPeriodo = models.AutoField(primary_key=True)
+    fechaInicio = models.DateField()
+    fechaFin = models.DateField()
+
+    class Meta: 
+        db_table = 'periodos'
+        managed = False
+    def __str__(self):
+        return str( self.idPeriodo)
+
+class Cuenta(models.Model):
+    idCuenta = models.AutoField(unique=True, primary_key=True)
+    codClase = models.IntegerField()
+    rubro = models.IntegerField()
+    codMayor = models.IntegerField()
+    codCuenta = models.TextField()
+    nameCuenta = models.TextField()
+    codDetalle = models.TextField(unique=True)
+
+    class Meta:
+        db_table = 'cuentas'
+        managed = False
+
+    def __str__(self):
+        return self.nameCuenta
+
+#MODELO DE REGISTRO DE TRANSACCIÓN   
+class Transaction(models.Model):
+    idTransaccion = models.AutoField(primary_key=True)
+    idPeriodo = models.ForeignKey('periodos', on_delete=models.CASCADE)
+    numPartida = models.CharField(max_length=50) 
+    idCuenta = models.ForeignKey(Cuenta, on_delete=models.CASCADE)
+    monto = models.DecimalField(max_digits=10, decimal_places=2)   
+    descripcion = models.TextField()
+    fecha = models.DateField()
+
+    class Meta:
+        db_table = 'gtransacciones_transaction'
+        ordering = ['fecha']
+
+    def __str__(self):
+        return f'Transacción {self.idTransaccion}'
+
+    def registrar_saldo(self, id_cuenta, monto, tipo, fecha):
+        cuenta = Cuenta.objects.get(id=id_cuenta)
+        saldo_transaccion = SaldosTransaccion(
+            idCuenta=id_cuenta,
+            idTransaccion=self,
+            fecha=fecha
+        )
+        if tipo.lower() == 'cargo':
+            saldo_transaccion.monto_cargo = monto
+        elif tipo.lower() == 'haber':
+            saldo_transaccion.monto_haber = monto
+        else:
+            raise ValueError("El tipo de saldo debe ser 'cargo' o 'haber'.")
+
+        saldo_transaccion.save()
+
+
+class SaldosTransaccion(models.Model):
+    idSaldoTransaccion = models.AutoField(primary_key=True)
+    idCuenta = models.ForeignKey('Cuenta', on_delete=models.CASCADE)
+    idTransaccion = models.ForeignKey('Transaction', on_delete=models.CASCADE)
+    monto_cargo = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    monto_haber = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    fecha = models.DateField()
+
+    class Meta:
+        db_table = 'gtransacciones_saldostransaccion'
+
+    def __str__(self):
+        return f'Saldo {self.idSaldoTransaccion} - Cuenta: {self.idCuenta}'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+from django.db import models
+
+# Create your models here.
+class periodos(models.Model) :
+    idPeriodo = models.AutoField(primary_key=True)
+    fechaInicio = models.DateField()
+    fechaFin = models.DateField()
+
+    class Meta: 
+        db_table = 'periodos'
+        managed = False
+    def __str__(self):
+        return str( self.idPeriodo)
+    
+class Transaction(models.Model):
+    idTransaccion = models.AutoField(unique=True, primary_key=True)
+    idPeriodo = models.ForeignKey('periodos', on_delete=models.CASCADE)
+    numPartida = models.CharField(max_length=50) 
+    idCuenta = models.ForeignKey('Cuenta', on_delete=models.CASCADE) 
+    monto = models.DecimalField(max_digits=10, decimal_places=2)  
+    #idSaldoTransaccion = models.ForeignKey('SaldoTransaccion', on_delete=models.CASCADE)
+    descripcion = models.TextField()
+    fecha = models.DateField()
+
+    class Meta:
+        db_table = 'gtransacciones_transaction'
+        ordering = ['fecha']
+
+    def __str__(self):
+        return f'Transacción {self.idTransaccion}'
+    
+class saldosTransaccion(models.Model):
+    idSaldoTransaccion = models.AutoField(unique=True, primary_key=True)
+    idCuenta = models.ForeignKey('Cuenta', on_delete=models.CASCADE)
+    idTransaccion = models.ForeignKey('Transaction', on_delete=models.CASCADE)
+    monto_cargo = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    monto_haber = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    fecha = models.DateField()
+
+    class Meta:
+        db_table = 'gtransacciones_saldostransaccion'
+
+    def __str__(self):
+        return f'Saldo Transacción {self.idSaldoTransaccion}'
+
+
+
+
+
+date = models.DateField()
     description = models.CharField(max_length=255)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     debit_account = models.ForeignKey(Account, related_name='debit_transactions', on_delete=models.CASCADE)
--- gtransacciones/views.py
+++ gtransacciones/views.py
@@ -1,17 +1,63 @@
 from django.shortcuts import render, redirect
+from .forms import TransactionForm, SaldosTransaccionFormSet
+from .models import Transaction, SaldosTransaccion
+
+def create_transaction(request):
+    if request.method == 'POST':
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            transaction = form.save()  # Guarda la transacción
+            saldos_data = request.POST.getlist('saldos')
+            
+            for saldo in saldos_data:
+                tipo = saldo.get('tipo')
+                id_cuenta = saldo.get('idCuenta')  # ID de cuenta
+                monto = saldo.get('monto')
+                fecha = saldo.get('fecha')  # Fecha del saldo
+
+                # Llama al método para registrar el saldo
+                transaction.registrar_saldo(id_cuenta, monto, tipo, fecha)
+                
+            return redirect('create_transaction')  # Redirige a la misma página para registrar otra transacción
+    else:
+        form = TransactionForm()
+    return render(request, 'create_transaction.html', {'form': form})
+
+
+
+
+
+
+
+
+
+"""from django.shortcuts import render, redirect
 from .forms import TransactionForm
+from .models import Transaction
 
 # Create your views here.
-"""def transacciones(request):
-    return render(request, 'rtransacciones.html')"""
+def transacciones(request):
+    return render(request, 'rtransacciones.html')
 
 
 def create_transaction(request):
     if request.method == 'POST':
         form = TransactionForm(request.POST)
         if form.is_valid():
-            form.save()
+            transaction = form.save()  # Guarda la transacción
+            saldos_data = request.POST.getlist('saldos')
+            
+            for saldo in saldos_data:
+                tipo = saldo.get('tipo')
+                id_cuenta = saldo.get('idCuenta')  # ID de cuenta
+                monto = saldo.get('monto')
+                fecha = saldo.get('fecha')  # Fecha del saldo
+
+                # Llama al método para registrar el saldo
+                transaction.registrar_saldo(id_cuenta, monto, tipo, fecha)
+                
+            #form.save()
             return redirect('create_transaction')  # Redirige a la misma página para registrar otra transacción
     else:
         form = TransactionForm()
-    return render(request, 'create_transaction.html', {'form': form})
+    return render(request, 'create_transaction.html', {'form': form})"""

e3ae8b2662969bbcc8bad7954f9f4c7b0744655b
ph22012 <ph22012@ues.edu.sv>
2024-11-05 17:36:13 -0600

    Se agregaron paginas preeliminares estados financieros

--- EstadosFinancieros/views.py
+++ EstadosFinancieros/views.py
@@ -35,3 +35,25 @@ def comprobacion(request):
 
     
     return render(request, 'comprobacion.html', {'periodoActual':periodoActual, 'saldosComprobacion':saldosComprobacion, 'sumDebe':sumDebe,'sumHaber':sumHaber})
+
+def resultados(request):
+
+    fechaHoy = date.today();
+    periodosContables = periodos.objects.all()
+    periodoActual = None
+    saldosAll = SaldosCuentas.objects.all()
+    sumDebe = 0
+    sumHaber =0
+
+    for periodo in periodosContables:
+        if fechaHoy >= periodo.fechaInicio and fechaHoy <= periodo.fechaFin:
+            periodoActual = periodo
+            break
+    
+    saldosResultado = saldosAll.exclude(idPeriodo__idPeriodo=periodoActual.idPeriodo)
+    for saldo in saldosResultado:
+        sumDebe += saldo.debe
+        sumHaber += saldo.haber
+
+    
+    return render(request, 'resultados.html', {'periodoActual':periodoActual, 'saldosResultado':saldosResultado, 'sumDebe':sumDebe,'sumHaber':sumHaber})

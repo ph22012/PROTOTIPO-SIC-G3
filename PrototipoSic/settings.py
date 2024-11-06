0750243ced73c871bd62d6e1798217aa2c0faaf9
ph22012 <ph22012@ues.edu.sv>
2024-11-05 23:44:12 -0600

    Fix

--- PrototipoSic/settings.py
+++ PrototipoSic/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.enviroment.get('SECRET_KEY', default='llavedefault123')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='llavedefault123')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ

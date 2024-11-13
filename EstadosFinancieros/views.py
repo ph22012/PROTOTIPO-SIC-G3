df07ab2aaa28f95e0bd1c0d7d828c7967a5bc7d7
ph22012 <ph22012@ues.edu.sv>
2024-11-12 21:17:29 -0600

    Fix 3

--- EstadosFinancieros/views.py
+++ EstadosFinancieros/views.py
@@ -10,7 +10,7 @@ def gestionar(request):
     fechaHoy = date.today();
     periodosContables = periodos.objects.all()
     periodoActual = None
-    request.session['periodoSelected']
+    request.session['periodoSelected'] = 2
     if request.method == 'POST':
         selected = request.POST.get('seleccion')
         request.session['periodoSelected'] = selected
